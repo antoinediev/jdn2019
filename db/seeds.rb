673c6c84a533ddfcb6fe801da5e27aed70b0d86f
dieval <antoine.dieval@gmail.com>
2019-10-20 21:41:34 +0200

    change db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,60 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin-jdn@ig2i.fr', password: 'oscar', password_confirmation: 'password') if Rails.env.development?
 
 %w[
-  Ceci
-  Est
-  Une
-  Liste
-  De
-  Tables
+  Fight Club
+  Pulp Fiction
+  Very Bad Trip
+  Blade Runner
+  Interstellar 
+  Le parrain
+  Inception
+  The dark knight 
+  Le seigneur des anneaux
+  Forrest Gump
+  Jumanji
+  Dikkenek
+  Apocalypse now
+  Retour vers le futur
+  Matrix
+  Le voyage de chihiro 
+  Pirate des caraibes
+  Into the wild 
+  Gladiator
+  Drive
+  Les évadés 
+  Les affranchis 
+  Taxi
+  La ligne verte 
+  Django Unchained
+  Shutter Island
+  Kill bill
+  Jurassic Park
+  Requiem for a dream
+  Inglourious Basterds
+  Asterix Mission Cléopatre
+  The truman Show
+  Titanic
+  Gran torino
+  Léon 
+  La cité de la peur 
+  Psychose 
+  American Pie
+  La haine
+  Le cinquième élément 
+  Terminator
+  Les temps modernes
+  OSS117 
 ].each do |name|
   Table.create(name: name, capacity: 10)
 end
 
 %w[
   normal
-  halal
   vegan
+  enfant
 ].each do |name|
   Menu.create(name: name, icon: 'fas fa-star-and-crescent')
 end
